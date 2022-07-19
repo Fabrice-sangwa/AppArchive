c94abfdf440d6ef319e003af50b29632404dff12
Fabrice-sangwa <19sm476@esisalama.org>
2022-07-19 19:00:49 +0200

    Finalisation de la première partie

--- app/src/main/java/org/esisalama/mobile/newapp/AjouterTravailActivity.java
+++ app/src/main/java/org/esisalama/mobile/newapp/AjouterTravailActivity.java
@@ -2,50 +2,91 @@ package org.esisalama.mobile.newapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AjouterTravailActivity extends AppCompatActivity {
 
+
     private EditText editTextDescription;
-    private EditText editTextPromotion ;
-    private EditText editTextCategorie;
+    private EditText editTextPromotion;
+    private EditText editTextCategory;
     private EditText editTextDateTravail;
-    private Button boutonAjouterphoto;
-    private Button boutonAjouterTravail;
-    private TextView textViewNomsPhotos;
+    private Button buttonAjouterPhoto;
+    private Button buttonAjouterTravail;
     private ProgressBar progressBar;
+    private TextView tvNomsPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_travail);
-
-        initialiserCompoant();
-
-
-
+        initialiserComposant();
+        verificationConfirmation();
     }
 
-    private void initialiserCompoant(){
+    private void initialiserComposant(){
 
         editTextDescription = findViewById(R.id.editTextDesc);
         editTextPromotion = findViewById(R.id.editTextPromotion);
-        editTextCategorie = findViewById(R.id.editTextCat);
-        editTextPromotion = findViewById(R.id.editTextPromotion);
+        editTextCategory = findViewById(R.id.editTextCat);
         editTextDateTravail = findViewById(R.id.editTextDate);
-        boutonAjouterphoto = findViewById(R.id.btnAjouterPhoto);
-        textViewNomsPhotos = findViewById(R.id.tvImages);
-        boutonAjouterTravail = findViewById(R.id.btnAjouterTravail);
+        buttonAjouterPhoto = findViewById(R.id.btnAjouterPhoto);
+        buttonAjouterTravail = findViewById(R.id.btnAjouterTravail);
         progressBar = findViewById(R.id.progressBar);
+        tvNomsPhoto = findViewById(R.id.tvImages);
+    }
 
+    private void verificationConfirmation(){
+        buttonAjouterTravail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = editTextDescription.getText().toString();
+                String promotion = editTextPromotion.getText().toString();
+                String categorie = editTextCategory.getText().toString();
+                String date = editTextDateTravail.getText().toString();
 
+                if (description.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champs description ne doit pas être vide ! ",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else if (promotion.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ promotion ne doit  pas être vide ! ",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else  if (categorie.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ categorie ne doit pas être vide !",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else if (date.isEmpty()){
+                    Toast.makeText(
+                            AjouterTravailActivity.this,
+                            "Le champ date ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }else {
+                    retourHomePage();
+                }
 
+            }
+        });
+    }
 
-
+    private void retourHomePage(){
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
     }
 
 
