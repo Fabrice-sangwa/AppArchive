7e62e673aee66e932e09e02b3eaaeb4f05fccd15
Fabrice-sangwa <19sm476@esisalama.org>
2022-07-21 11:06:42 +0200

    Modification de l'activité modifier

--- app/src/main/java/org/esisalama/mobile/newapp/ProfileActivity.java
+++ app/src/main/java/org/esisalama/mobile/newapp/ProfileActivity.java
@@ -1,8 +1,13 @@
 package org.esisalama.mobile.newapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.menu_modifier_profile){
+            Toast.makeText(
+                    ProfileActivity.this,
+                    "Menu Modifier",
+                    Toast.LENGTH_LONG
+            ).show();
+            Intent intent = new Intent(ProfileActivity.this,ModifierProfileActivity.class);
+            startActivity(intent);
+
+
+        } else if (item.getItemId() == R.id.menu_parametre) {
+            Toast.makeText(
+
+                    ProfileActivity.this,
+                    "Menu Paramètre",
+                    Toast.LENGTH_LONG
+            ).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
